5e83b9129a76b1ddc0340ff9386035c7feee86be
Kosi Akporji <kosiazom@gmail.com>
2020-04-29 16:06:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
-  planeteer_calls.any? do |calls, index|
+ 
+  planeteer_calls.any? do |calls|
     calls.length > 4
   end
 end
