bccc951e025b36a48163391c0973e52ed3504341
Kosi Akporji <kosiazom@gmail.com>
2020-04-29 15:20:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
 
-  planeteer_calls.map do |calls|
+  planeteer_calls.map do |calls, index|
     p "#{calls.capitalize} !"
   end 
   planeteer_calls
