b61cba426fb8b3e39839ebeb9c3e56ca4f8b40a2
Kosi Akporji <kosiazom@gmail.com>
2020-04-29 16:17:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
-  planeteer_calls.include? do |calls|
+  planeteer_calls.find? do |calls|
     calls == valid_calls
   end
 end
